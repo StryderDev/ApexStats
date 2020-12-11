245fe9a02ec62e3d7c8bc93c84746165e589b64a
SDCore <sdcored@gmail.com>
2020-12-10 23:14:21 -0600

    Added Commands Command

---
+++ commands/commands.js
@@ -0,0 +1,38 @@
+const { client, Discord } = require("../ApexStats.js");
+require("dotenv").config();
+const config = require("../config.json");
+
+module.exports = {
+  name: "commands",
+  description: "Commands available to the bot.",
+  execute(message, args) {
+    const info = new Discord.MessageEmbed()
+      .setTitle("Apex Legends Stats Bot")
+      .setColor("C21D27")
+      .setThumbnail(process.env.BOT_ICON)
+      .addField(
+        `${config.prefix}stats [platform] [username]`,
+        "Shows user stats. Platform can be PC, X1, or PS4."
+      )
+      .addField(
+        `${config.prefix}status`,
+        "Shows current Apex Legends server status.",
+        true
+      )
+      .addField(
+        `${config.prefix}news`,
+        "Shows the latest news article from the official Apex Legends blog.",
+        true
+      )
+      .addField(
+        `${config.prefix}info`,
+        "Shows basic information about the bot.",
+        true
+      )
+      .addField(`${config.prefix}commands`, "Shows this embed.", true)
+      .setFooter(process.env.CREATOR_NAME, process.env.CREATOR_LOGO)
+      .setTimestamp();
+
+    message.channel.send(info);
+  },
+};
