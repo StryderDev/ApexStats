45b76cfc8ec7b03f4e8f628fdec3fed3e97949ae
SDCore <sdcored@gmail.com>
2020-12-19 01:43:58 -0600

    More Work on Map Command
    - Added name of current map
    - Added name of next map in rotation along with time until that map
    - Added image of current map

--- commands/map.js
+++ commands/map.js
@@ -1,9 +1,7 @@
 const { client, Discord } = require("../ApexStats.js");
 require("dotenv").config();
-const config = require("../config.json");
-const fs = require("fs");
-const moment = require("moment"); //replace
 const axios = require("axios");
+var { DateTime, Duration } = require("luxon");
 
 module.exports = {
   name: "map",
@@ -15,9 +13,43 @@ module.exports = {
         axios
           .get("https://fn.alphaleagues.com/v1/apex/map/")
           .then((res) => {
-            const map = new Discord.MessageEmbed().setDescription(
-              `The current map is **${res.data.map}**`
-            );
+            function mapImage(name) {
+              if (name == "Olympus") {
+                return `Olympus.png?q=${DateTime.local().toFormat("X")}`;
+              } else if (name == "World's Edge") {
+                return `WorldsEdge.png?q=${DateTime.local().toFormat("X")}`;
+              } else {
+                return `NoMapData.png?q=${DateTime.local().toFormat("X")}`;
+              }
+            }
+
+            function nextMap(name) {
+              if (name == "Olympus") {
+                return "World's Edge";
+              } else if (name == "World's Edge") {
+                return "Olympus";
+              }
+            }
+
+            function time(seconds) {
+              return Duration.fromObject({ seconds: seconds }).toFormat(
+                "mm 'minutes' ss 'seconds'"
+              );
+            }
+
+            const map = new Discord.MessageEmbed()
+              .setDescription(
+                `The current map is **${
+                  res.data.map
+                }**.\nThe next map in rotation is **${nextMap(
+                  res.data.map
+                )}** in **${time(res.data.times.remaining.seconds)}.**`
+              )
+              .setImage(
+                `https://sdcore.dev/cdn/ApexStats/Maps/${mapImage(
+                  res.data.map
+                )}`
+              );
 
             msg.delete();
             msg.channel.send(map);
