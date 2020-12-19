cc0e5379c9ff01553796dff08dd2a9060c11302c
SDCore <sdcored@gmail.com>
2020-12-19 00:18:35 -0600

    Installing Axios for API Calls, Began Work on Map Command
    - Using Axios to call data from map rotation API
    - Added placeholder map images for map rotation
    - Base work for calling to map rotation API and returning data

--- commands/map.js
+++ commands/map.js
@@ -3,11 +3,28 @@ require("dotenv").config();
 const config = require("../config.json");
 const fs = require("fs");
 const moment = require("moment"); //replace
+const axios = require("axios");
 
 module.exports = {
   name: "map",
   description: "Shows current map rotation.",
   execute(message, args) {
-    console.log("current map is :D");
+    message.channel
+      .send("Getting current map rotation schedule...")
+      .then(async (msg) => {
+        axios
+          .get("https://fn.alphaleagues.com/v1/apex/map/")
+          .then((res) => {
+            const map = new Discord.MessageEmbed().setDescription(
+              `The current map is **${res.data.map}**`
+            );
+
+            msg.delete();
+            msg.channel.send(map);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      });
   },
 };
