159a38040b4201e7b6caa9438cd3a198720f4148
SDCore <sdcored@gmail.com>
2020-12-19 23:46:04 -0600

    Apex Map Rotation Auto Updating Message

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,7 @@
-const { client } = require("../ApexStats.js");
-var { DateTime } = require("luxon");
+const { client, Discord } = require("../ApexStats.js");
+var { DateTime, Duration } = require("luxon");
 const config = require("../config.json");
+const axios = require("axios");
 
 client.once("ready", () => {
   console.log(`Logging in as ${client.user.tag}`);
@@ -36,4 +37,89 @@ client.once("ready", () => {
       }`
     );
   }, Math.max(1, 30 || 1) * 60 * 1000);
+
+  // ----- APEX MAP ROTATION UPDATE ----- //
+  function updateMapRotation() {
+    axios
+      .get("https://fn.alphaleagues.com/v1/apex/map/")
+      .then((res) => {
+        function mapImage(name) {
+          if (name == "Olympus") {
+            return `Olympus.png?q=${DateTime.local().toFormat("X")}`;
+          } else if (name == "World's Edge") {
+            return `WorldsEdge.png?q=${DateTime.local().toFormat("X")}`;
+          } else {
+            return `NoMapData.png?q=${DateTime.local().toFormat("X")}`;
+          }
+        }
+
+        function nextMap(name) {
+          if (name == "Olympus") {
+            return "World's Edge";
+          } else if (name == "World's Edge") {
+            return "Olympus";
+          }
+        }
+
+        function time(seconds) {
+          return Duration.fromObject({ seconds: seconds }).toFormat(
+            "mm 'minutes' ss 'seconds'"
+          );
+        }
+
+        const mapRotation = new Discord.MessageEmbed()
+          .setDescription(
+            `The current map is **${
+              res.data.map
+            }**.\nThe next map in rotation is **${nextMap(
+              res.data.map
+            )}** in **${time(res.data.times.remaining.seconds)}.**`
+          )
+          .setImage(
+            `https://sdcore.dev/cdn/ApexStats/Maps/${mapImage(res.data.map)}`
+          );
+
+        const guild = client.guilds.cache.get(config.mapRotation.guildID);
+        if (!guild) return console.log("Unable to find guild.");
+
+        const channel = guild.channels.cache.find(
+          (c) => c.id === config.mapRotation.channelID && c.type === "text"
+        );
+        if (!channel) return console.log("Unable to find channel.");
+
+        try {
+          const message = channel.messages.fetch(config.mapRotation.messageID);
+          if (!message) return console.log("Unable to find message.");
+
+          channel.messages.fetch(config.mapRotation.messageID).then((mesg) => {
+            mesg.edit(mapRotation);
+          });
+        } catch (err) {
+          console.error(err);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  updateMapRotation();
+  console.log(
+    `[${DateTime.local().toFormat("hh:mm:ss")}] Updated Map Rotation Embed`
+  );
+
+  // Update the bot presence every 30 minutes to update
+  // the amount of servers the bot is in
+  setInterval(function () {
+    var date = new Date();
+
+    console.log("Checking update interval...");
+
+    if (date.getMinutes() % config.mapRotation.updateInterval == 0) {
+      updateMapRotation();
+      console.log(
+        `[${DateTime.local().toFormat("hh:mm:ss")}] Updated Map Rotation Embed`
+      );
+    }
+  }, Math.max(1, 1 || 1) * 60 * 1000);
 });
