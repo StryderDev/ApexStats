c2babdf5a50746b7ee313295ef2324b8d3ccd383
SDCore <sdcored@gmail.com>
2020-12-24 02:20:26 -0600

    Small Changes to Base Files and Config

--- ShardManager.js
+++ ShardManager.js
@@ -2,10 +2,10 @@ require("dotenv").config;
 
 const config = require("./config.json");
 
-// Just using the ShardManager now so I don't have
-// to worry about it in the future. Will probably regret
-// it if I don't plan on making the bot public, but as
-// it stands right now, I do. Guess we'll see ;)
+// Just using the ShardManager now so I don't have to worry
+// about it in the future. Will probably regret it if I don't
+// plan on making the bot public, but as it stands right now,
+// Guess we'll see ;)
 
 const { ShardingManager } = require("discord.js-light");
 const manager = new ShardingManager("./ApexStats.js", {
