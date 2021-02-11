fc392e2cebe9badae434fe979f97c2114cf5433a
SDCore <sdcored@gmail.com>
2021-02-10 21:35:52 -0600

    Some Changes for the Apex server

--- events/message.js
+++ events/message.js
@@ -1,12 +1,10 @@
-const { client, Discord } = require("../ApexStats.js");
+const {client, Discord} = require("../ApexStats.js");
 const config = require("../config.json");
 const fs = require("fs");
 
 client.commands = new Discord.Collection();
 
-const commandFiles = fs
-  .readdirSync("./commands")
-  .filter((file) => file.endsWith(".js"));
+const commandFiles = fs.readdirSync("./commands").filter((file) => file.endsWith(".js"));
 
 for (const file of commandFiles) {
   const command = require(`../commands/${file}`);
@@ -14,6 +12,7 @@ for (const file of commandFiles) {
 }
 
 client.on("message", (message) => {
+  // If the bot is DM'd, send a message to the support server
   if (message.channel.type == "dm" && !message.author.bot) {
     message.reply(
       `Hey! Join the support server at ${process.env.SUPPORT_SERVER} to get support.\n\n*These DM's are not monitored.*`
@@ -35,14 +34,37 @@ client.on("message", (message) => {
   const args = message.content.slice(config.prefix.length).trim().split(/ +/);
   const commandName = args.shift().toLowerCase();
 
-  if (!client.commands.has(commandName))
-    return console.log("User did not run a valid command.");
+  if (!client.commands.has(commandName)) return console.log("User did not run a valid command.");
   const command = client.commands.get(commandName);
 
-  try {
-    command.execute(message, args);
-  } catch (error) {
-    console.log(error);
-    // message.reply("there was an issue running that command.");
+  // If server command is sent in is the Apex server
+  if (message.guild.id == "541484311354933258") {
+    // If channel command is sent in is #use-bots-here
+    if (message.channel.id == "632283707482308608") {
+      try {
+        command.execute(message, args);
+      } catch (error) {
+        return console.log(`Error: ${error}`);
+      }
+    } else if (
+      message.member.roles.cache.some((role) => role.name === "Discord Moderator") ||
+      message.member.roles.cache.some((role) => role.name === "Admin")
+    ) {
+      // User has Discord Moderator or Admin roles and can send a message
+      try {
+        command.execute(message, args);
+      } catch (error) {
+        return console.log(`Error: ${error}`);
+      }
+    } else {
+      // Outside of inteded channel in server
+      return console.log("User is not allowed to do that command here.");
+    }
+  } else {
+    try {
+      command.execute(message, args);
+    } catch (error) {
+      return console.log(`Error: ${error}`);
+    }
   }
 });
