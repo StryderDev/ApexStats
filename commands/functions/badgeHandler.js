c6d3e4c66e8705d8c85d1eb168a75f86ce30aada
SDCore <sdcored@gmail.com>
2021-03-21 17:52:07 -0500

    Badge Notes

--- commands/functions/badgeHandler.js
+++ commands/functions/badgeHandler.js
@@ -3,3 +3,12 @@ function findBadgeByID(id) {
 }
 
 module.exports = {findBadgeByID};
+
+// BADGE NOTES
+//
+// Default Badge Icon
+// https://apexlegends.fandom.com/wiki/File:CosmeticIcon_Badges.png
+// - Might have to recreate a higher resolution version for the bot
+//
+// Default BadgeID for empty badge slots
+// 1488777442
