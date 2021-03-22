093152607c9fe5a0a59db4320432f9f8a7b82ff0
SDCore <sdcored@gmail.com>
2021-03-21 19:30:27 -0500

    More Badge Placeholders + Notes

--- commands/functions/badgeHandler.js
+++ commands/functions/badgeHandler.js
@@ -6,6 +6,28 @@ module.exports = {findBadgeByID};
 
 // BADGE NOTES
 //
+// Main idea is:
+// Step 1.
+// Check if the ID of the badge is one of the following:
+// - Account Badge
+// - BattlePass Badge
+// - Club Badge
+// - Content Pack Badge
+// - GameMode Badge
+// - Ranked Badge
+// If it is, return the nessecary info
+// If it isn't, get the current character, check their related
+//  badge json file, and return the nessecary info
+// Step 2.
+// Associate the badge ID with 2 things:
+// 1. Emote ID (gonna need a lot of emotes... and servers...)
+// 2. Emote Name (to associate the ID with the emote on discord)
+// Step 3.
+// Return badge emote and, if legend specific badge, name
+//
+// If there is no ID currently in the files, return
+// "No Data" and "-", similar to trackers
+//
 // Default Badge Icon
 // https://apexlegends.fandom.com/wiki/File:CosmeticIcon_Badges.png
 // - Might have to recreate a higher resolution version for the bot
