081df5ef266185219600f9ef250c71e57b761dd1
SDCore <sdcored@gmail.com>
2021-03-30 03:35:28 -0500

    Updated compact command to work with pred rankings

--- commands/compact.js
+++ commands/compact.js
@@ -1,24 +1,8 @@
 const {Discord} = require("../ApexStats.js");
 const config = require("../config.json");
 const percentage = require("percentagebar");
-const legends = require("../GameData/legends.json");
-const colours = require("../GameData/legendColors.json");
 
-let mysql = require("mysql");
-let connection = mysql.createPool({
-  host: config.APISQL.host,
-  user: config.APISQL.username,
-  password: config.APISQL.password,
-  database: config.APISQL.database,
-});
-
-var {DateTime} = require("luxon");
 const {default: axios} = require("axios");
-
-var currentTimestamp = DateTime.local().toFormat("ooo") * 2;
-
-var OnlineEmoji = "<:StatusUp:786800700533112872>";
-var OfflineEmoji = "<:StatusDown:786800700201238570>";
 
 module.exports = {
   name: "compact",
@@ -73,200 +57,109 @@ module.exports = {
         "Sorry, it looks like you didn't provide a valid platform.\nFor reference, PC = Origin/Steam, X1 = Xbox, and PS4 = Playstation Network."
       );
 
-    function checkPlat(platform, username) {
-      if (platform == "PC") {
-        return username;
-      } else {
-        return "SDCore";
-      }
-    }
-
     function formatNumbers(number) {
       return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     }
 
-    rexxURL = `https://api.apexstats.dev/v5.php?platform=${platformCheck}&player=${player}`;
     mainURL = `https://api.apexstats.dev/v5.php?platform=${platformCheck}&player=${player}`;
 
-    var rexx = axios.get(rexxURL);
-    var main = axios.get(mainURL);
+    message.channel
+      .send("Retrieving stats...")
+      .then(async (msg) => {
+        axios.get(mainURL).then(function (response) {
+          // Response Data
+          var mainResponse = response.data;
 
-    message.channel.send("Retrieving stats...").then(async (msg) => {
-      axios
-        .all([rexx, main])
-        .then(
-          axios.spread((...responses) => {
-            // Response Data
-            var rexxResponse = responses[0].data;
-            var mainResponse = responses[1].data;
+          // Season/Account Info
+          var season = "8";
+          var currentRank = mainResponse.accountInfo.ranked;
+          var accountBP = mainResponse.accountInfo.battlepass.level;
+          var accountLevel = mainResponse.accountInfo.level;
 
-            // Season/Account Info
-            var season = "8";
-            var avatar = "https://cdn.apexstats.dev/Icon.png";
-            if (platformCheck == "PS4") {
-              var userID = mainResponse.userData.userID.toString().slice(0, -5);
+          function getAccountLevel(level) {
+            if (level >= 500) {
+              return 500;
             } else {
-              var userID = mainResponse.userData.userID;
+              return level;
             }
-            var userName = mainResponse.userData.username;
-            var userPlatform = mainResponse.userData.platform;
-            var isOnline = mainResponse.userData.online;
-            var selectedLegend = mainResponse.accountInfo.active.legend;
-            var currentRank = mainResponse.accountInfo.ranked;
-            var accountBP = mainResponse.accountInfo.battlepass.level;
-            var accountLevel = mainResponse.accountInfo.level;
-            var lastUpdated = Math.floor(new Date().getTime() / 1000);
+          }
 
-            let checkQuery = `SELECT * FROM \`users\` WHERE \`PlayerID\` = '${userID}';`;
-            let insertQuery = `INSERT INTO \`users\` (\`PlayerID\`, \`PlayerName\`, \`Platform\`, \`Level\`, \`RankScore\`, \`lastUpdated\`) VALUES ('${userID}', '${userName}', '${userPlatform}', '${accountLevel}', '${currentRank.score}', '${lastUpdated}')`;
-
-            connection.getConnection(function (err, connection) {
-              if (err) {
-                console.log(err);
-                return message.channel.send(
-                  "There was an error connecting to the database. Please try again later."
-                );
-              }
-
-              connection.query(checkQuery, function (err, results) {
-                if (err) {
-                  connection.release();
-                  console.log(err);
-                  return message.channel.send(
-                    "There was a problem with the SQL syntax. Please try again later."
-                  );
-                }
-
-                if (results.length > 0) {
-                  // console.log("found user.");
-                } else {
-                  connection.query(insertQuery, function (err, results) {
-                    console.log(err);
-                  });
-                }
-
-                connection.release();
-              });
-            });
-
-            function findLegendByID() {
-              var getLegend = legends[mainResponse.accountInfo.active.legend];
-
-              if (getLegend == "undefined" || getLegend == null) {
-                return "NoBanner";
+          function getAccountBP(BP) {
+            if (BP != -1) {
+              if (BP >= 110) {
+                return 110;
               } else {
-                return getLegend;
+                return BP;
               }
+            } else {
+              return 0;
+            }
+          }
+
+          function getRankBadge(rankName) {
+            if (rankName == "Silver") {
+              return "<:rankedSilver:787174770424021083>";
+            } else if (rankName == "Gold") {
+              return "<:rankedGold:787174769942462474>";
+            } else if (rankName == "Platinum") {
+              return "<:rankedPlatinum:787174770780667944>";
+            } else if (rankName == "Diamond") {
+              return "<:rankedDiamond:787174769728290816>";
+            } else if (rankName == "Master") {
+              return "<:rankedMaster:787174770680135680>";
+            } else if (rankName == "Predator" || rankName == "Apex Predator") {
+              return "<:rankedPredator:787174770730336286>";
+            } else {
+              return "<:rankedBronze:787174769623302204>";
+            }
+          }
+
+          function findRank(rankName, ladderPos, rankDiv) {
+            if (rankName == "Apex Predator") {
+              var rank = `[#${ladderPos}] Apex Predator`;
+            } else {
+              var rank = `${rankName} ${rankDiv}`;
             }
 
-            function getAccountLevel(level) {
-              if (level >= 500) {
-                return 500;
-              } else {
-                return level;
-              }
-            }
+            return `${getRankBadge(rankName)} ${rank}`;
+          }
 
-            function getAccountBP(BP) {
-              if (BP != -1) {
-                if (BP >= 110) {
-                  return 110;
-                } else {
-                  return BP;
-                }
-              } else {
-                return 0;
-              }
-            }
+          // Main Stats Embed
+          const statsMain = new Discord.MessageEmbed()
+            .setTitle(`Stats for ${mainResponse.userData.username} on ${platformUppercase}`)
+            .addField(
+              "Account Stats",
+              `**Rank**\n${findRank(
+                currentRank.name,
+                currentRank.ladderPOS,
+                currentRank.division
+              )}\n**Score**\n${formatNumbers(currentRank.score)}`,
+              true
+            )
+            .addField(
+              `Account/S${season} BP Level`,
+              `**Account Level ${accountLevel.toLocaleString()}/500**\n${percentage(
+                500,
+                getAccountLevel(accountLevel),
+                10
+              )}\n**BattlePass Level ${getAccountBP(accountBP)}/110**\n${percentage(
+                110,
+                getAccountBP(accountBP),
+                10
+              )}`,
+              true
+            );
 
-            function getRankBadge(rankName) {
-              if (rankName == "Silver") {
-                return "<:rankedSilver:787174770424021083>";
-              } else if (rankName == "Gold") {
-                return "<:rankedGold:787174769942462474>";
-              } else if (rankName == "Platinum") {
-                return "<:rankedPlatinum:787174770780667944>";
-              } else if (rankName == "Diamond") {
-                return "<:rankedDiamond:787174769728290816>";
-              } else if (rankName == "Master") {
-                return "<:rankedMaster:787174770680135680>";
-              } else if (rankName == "Predator" || rankName == "Apex Predator") {
-                return "<:rankedPredator:787174770730336286>";
-              } else {
-                return "<:rankedBronze:787174769623302204>";
-              }
-            }
-
-            function getTrackerTitle(id, legend) {
-              if (id == "1905735931") {
-                return "No data";
-              } else {
-                var trackerFile = require(`../GameData/TrackerData/${legend}.json`);
-
-                if (trackerFile[id] == "undefined" || trackerFile[id] == null) {
-                  return id;
-                } else {
-                  return trackerFile[id];
-                }
-              }
-            }
-
-            function getTrackerValue(id, value) {
-              if (id == "1905735931") {
-                return "-";
-              } else {
-                return value;
-              }
-            }
-
-            function getUserStatus() {
-              if (isOnline == "1") {
-                return `**Status:** ${OnlineEmoji}Online`;
-              } else {
-                return `**Status:** ${OfflineEmoji}Offline`;
-              }
-            }
-
-            // Main Stats Embed
-            const statsMain = new Discord.MessageEmbed()
-              .setTitle(
-                `Stats for ${
-                  mainResponse.userData.username
-                } on ${platformUppercase} playing ${findLegendByID(selectedLegend)}`
-              )
-              .setDescription(getUserStatus())
-              .addField(
-                "Ranked Placement",
-                `**Rank:** ${getRankBadge(currentRank.name)} ${currentRank.name} ${
-                  currentRank.division
-                }\n**Score:** ${formatNumbers(currentRank.score)}`,
-                true
-              )
-              .addField(
-                `Account & S${season} BattlePass Level`,
-                `**Account Level ${accountLevel.toLocaleString()}/500**\n${percentage(
-                  500,
-                  getAccountLevel(accountLevel),
-                  10
-                )}\n**BattlePass Level ${getAccountBP(accountBP)}/110**\n${percentage(
-                  110,
-                  getAccountBP(accountBP),
-                  10
-                )}`,
-                true
-              );
-
-            msg.delete();
-            msg.channel.send(statsMain);
-          })
-        )
-        .catch((errors) => {
-          console.log(`Error: ${errors}`);
           msg.delete();
-          message.channel.send(
-            "There was an error looking up that username. If you're on PC, try your origin username. If the problem persists, contact support."
-          );
+          msg.channel.send(statsMain);
         });
-    });
+      })
+      .catch((errors) => {
+        console.log(`Error: ${errors}`);
+        msg.delete();
+        message.channel.send(
+          "There was an error looking up that username. If you're on PC, try your origin username. If the problem persists, contact support."
+        );
+      });
   },
 };
