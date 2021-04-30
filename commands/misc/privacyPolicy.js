28fd0c9341cf2ad4b4b65f209162f1ee45540990
SDCore <sdcored@gmail.com>
2021-04-30 03:02:21 -0500

    Small Updates

--- commands/misc/privacyPolicy.js
+++ commands/misc/privacyPolicy.js
@@ -1,17 +1,6 @@
 const chalk = require("chalk");
 const {Command} = require("discord.js-light-commando");
-const {MessageEmbed} = require("discord.js");
 const {checkMsg} = require("../functions/checkMsg.js");
-const config = require("../../config.json");
-
-let mysql = require("mysql");
-let connection = mysql.createPool({
-  host: config.SQL.host,
-  user: config.SQL.username,
-  password: config.SQL.password,
-  database: config.SQL.database,
-});
-
 module.exports = class MapCommand extends Command {
   constructor(client) {
     super(client, {
@@ -25,7 +14,7 @@ module.exports = class MapCommand extends Command {
   onError(error) {
     console.log(chalk`{red Error: ${error}}`);
   }
-  run(msg, {name}) {
+  run(msg) {
     if (checkMsg(msg) == 1) return;
 
     msg.say("Privacy Policy: https://apexstats.dev/privacypolicy.php");
--- oldCommands/privacypolicy.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: "privacypolicy",
-  description: "Shows the privacy policy.",
-  execute(message) {
-    message.channel.send(
-      "Privacy Policy: https://apexstats.dev/privacypolicy.html"
-    );
-  },
-};
