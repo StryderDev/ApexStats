923923d0ae0ed1cd8b2c8a28415c9a3851e5f611
SDCore <sdcored@gmail.com>
2021-05-02 00:35:17 -0500

    Invite command

--- oldCommands/invite.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: "invite",
-  description: "Invite the bot to your server.",
-  execute(message) {
-    message.channel.send(
-      "Invite the bot to your server: <https://apexstats.dev/invite>"
-    );
-  },
-};
