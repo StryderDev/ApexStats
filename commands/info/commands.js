5f4350ecc9e384654f28b13b7088a4191e461585
SDCore <sdcored@gmail.com>
2021-05-03 16:33:10 -0500

    Event and Commands Command

---
+++ commands/info/commands.js
@@ -0,0 +1,32 @@
+const chalk = require("chalk");
+const {MessageEmbed} = require("discord.js");
+const {Command} = require("discord.js-light-commando");
+const {checkMsg} = require("../functions/checkMsg.js");
+
+module.exports = class MapCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: "commands",
+      group: "info",
+      memberName: "commands",
+      description: "Shows bot commands.",
+      examples: ["info"],
+    });
+  }
+  onError(error) {
+    console.log(chalk`{red Error: ${error}}`);
+  }
+  run(msg) {
+    if (checkMsg(msg) == 1) return;
+
+    const embed = new MessageEmbed()
+      .setTitle("Bot Commands")
+      .addField(
+        "Commands",
+        ">>drop\n>>who\n>>commands\n>>legend\n>>news\n>>stats\n>>info\n>>invite\n>>privacypolicy\n>>map\n>>status",
+        true
+      );
+
+    msg.say(embed);
+  }
+};
