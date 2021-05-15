371cb32f7251069d5e5e401d931c93cb7915e430
SDCore <sdcored@gmail.com>
2021-05-15 16:10:21 -0500

    Added TopGG API

--- events/ready.js
+++ events/ready.js
@@ -2,6 +2,18 @@ const {client} = require("../ApexStats.js");
 const chalk = require("chalk");
 const {DateTime} = require("luxon");
 
+// Top.GG API
+const DBL = require("dblapi.js");
+
+if (config.topGG == "0") {
+  // Don't send data to TopGG
+} else {
+  const dbl = new DBL(config.topGG, client);
+  dbl.on("posted", () => {
+    console.log("Server count posted!");
+  });
+}
+
 client.once("ready", () => {
   console.log(chalk`{yellow [${DateTime.local().toFormat("hh:mm:ss")}] Logging in...}`);
   console.log(
