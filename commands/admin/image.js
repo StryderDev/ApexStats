50c66ff7e7f5885c778abaa795af0ba244bb3107
SDCore <sdcored@gmail.com>
2021-05-31 01:34:54 -0500

    Error Handling for Image Command + Tracker Updates
    - Errors from stats command implimented into image command for invalid platform/username/other errors
    - Removed default tracker image from tracker list, trackerImage() function in image command handles it automatically if there is not one demined

--- commands/admin/image.js
+++ commands/admin/image.js
@@ -84,12 +84,19 @@ module.exports = class MapCommand extends Command {
       );
 
     function trackerImage(id, legend) {
+      // Empty Tracker
       if (id == "1905735931") return "Default";
 
+      // Get tracker file of legend
       var tracker = require(`../../GameData/TrackerData/${legend}.json`);
 
-      if (tracker[id] == "undefined" || tracker[id] == null) return id;
+      // If there is no tracker file with the ID, return Default
+      if (tracker[id] == "undefined" || tracker[id] == null) return "Default";
 
+      // If there is no tracker image defined, return Default
+      if (tracker[id].Image == "undefined" || tracker[id].Image == null) return "Default";
+
+      // Return tracker image
       return tracker[id].Image;
     }
 
@@ -99,194 +106,238 @@ module.exports = class MapCommand extends Command {
     )}&player=${encodeURIComponent(username)}`;
 
     msg.say(`Retrieving user stats for ${username}...`).then(async (msg) => {
-      axios.get(APIURL).then(async function (response) {
-        // Create Canvas with info
-        // Username
-        Canvas.registerFont("fonts/blocktastic.otf", {family: "blocktastic"});
-        // Tracker Titles
-        Canvas.registerFont("fonts/OpenSans-Regular.ttf", {family: "OpenSans"});
-        Canvas.registerFont("fonts/OpenSans-Bold.ttf", {family: "OSBold"});
-        Canvas.registerFont("fonts/OpenSans-Light.ttf", {family: "OSLight"});
-        // Tracker Values
-        Canvas.registerFont("fonts/Cousine.ttf", {family: "Cousine"});
+      axios
+        .get(APIURL)
+        .then(async function (response) {
+          // Create Canvas with info
+          // Username
+          Canvas.registerFont("fonts/blocktastic.otf", {family: "blocktastic"});
+          // Tracker Titles
+          Canvas.registerFont("fonts/OpenSans-Regular.ttf", {family: "OpenSans"});
+          Canvas.registerFont("fonts/OpenSans-Bold.ttf", {family: "OSBold"});
+          Canvas.registerFont("fonts/OpenSans-Light.ttf", {family: "OSLight"});
+          // Tracker Values
+          Canvas.registerFont("fonts/Cousine.ttf", {family: "Cousine"});
 
-        const canvas = Canvas.createCanvas(500, 700);
-        const ctx = canvas.getContext("2d");
-        const background = await Canvas.loadImage(
-          "https://cdn.apexstats.dev/CanvasTesting/014.png"
-        );
-
-        // Draw background banner image
-        ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-
-        console.log("-- Fetching user data --");
-        var response = response.data;
-        console.log("-- Data fetched, parsing --");
-
-        // Grab info for banner
-        var username = response.userData.username;
-        var legend = response.accountInfo.active.legend;
-
-        // Badges
-        var badge = response.accountInfo.active;
-        var bOne = badge.badges[0];
-        var bTwo = badge.badges[1];
-        var bThree = badge.badges[2];
-
-        // Trackers
-        var tracker = response.accountInfo.active;
-        var one = tracker.trackers[0];
-        var two = tracker.trackers[1];
-        var three = tracker.trackers[2];
-
-        // Display user info on banner
-        ctx.fillStyle = "#ffffff";
-        ctx.font = "28px blocktastic";
-        ctx.textAlign = "center";
-        let user = truncate(username, 15);
-        ctx.fillText(user, 127, 57);
-
-        // Legend Image
-        const legendImage = await Canvas.loadImage(
-          `https://cdn.apexstats.dev/CanvasTesting/Legends/${findLegendByID(legend)}.png`
-        );
-        ctx.drawImage(legendImage, 0, 0);
-
-        // Top Badge
-        const badgeOneImage = await Canvas.loadImage(
-          `https://cdn.apexstats.dev/Badges/${badgeImage(bOne.id, bOne.value)}`
-        );
-        ctx.drawImage(badgeOneImage, 80, 86, 95, 95);
-
-        // Top Badge Value
-        if (hasValue(bOne.id, bOne.value)) {
-          const textOneBackground = await Canvas.loadImage(
-            `https://cdn.apexstats.dev/Badges/textBackground.png`
+          const canvas = Canvas.createCanvas(500, 700);
+          const ctx = canvas.getContext("2d");
+          const background = await Canvas.loadImage(
+            "https://cdn.apexstats.dev/CanvasTesting/014.png"
           );
-          ctx.drawImage(textOneBackground, 80, 86, 95, 95);
 
-          ctx.fillStyle = "#FFFFFFF";
-          ctx.font = "13px OSBold";
+          // Draw background banner image
+          ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+          console.log("-- Fetching user data --");
+          var response = response.data;
+          console.log("-- Data fetched, parsing --");
+
+          // Grab info for banner
+          var username = response.userData.username;
+          var legend = response.accountInfo.active.legend;
+
+          // Badges
+          var badge = response.accountInfo.active;
+          var bOne = badge.badges[0];
+          var bTwo = badge.badges[1];
+          var bThree = badge.badges[2];
+
+          // Trackers
+          var tracker = response.accountInfo.active;
+          var one = tracker.trackers[0];
+          var two = tracker.trackers[1];
+          var three = tracker.trackers[2];
+
+          // Display user info on banner
+          ctx.fillStyle = "#ffffff";
+          ctx.font = "28px blocktastic";
           ctx.textAlign = "center";
-          let oneTitle = getValue(bOne.id, bOne.value);
-          ctx.fillText(oneTitle, 127, 170);
-        }
+          let user = truncate(username, 15);
+          ctx.fillText(user, 127, 57);
 
-        // Middle Badge
-        const badgeTwoImage = await Canvas.loadImage(
-          `https://cdn.apexstats.dev/Badges/${badgeImage(bTwo.id, bTwo.value)}`
-        );
-        ctx.drawImage(badgeTwoImage, 80, 201, 95, 95);
-
-        // Middle Badge Value
-        if (hasValue(bTwo.id, bTwo.value)) {
-          const textTwoBackground = await Canvas.loadImage(
-            `https://cdn.apexstats.dev/Badges/textBackground.png`
+          // Legend Image
+          const legendImage = await Canvas.loadImage(
+            `https://cdn.apexstats.dev/CanvasTesting/Legends/${findLegendByID(legend)}.png`
           );
-          ctx.drawImage(textTwoBackground, 80, 201, 95, 95);
+          ctx.drawImage(legendImage, 0, 0);
 
-          ctx.fillStyle = "#FFFFFFF";
-          ctx.font = "13px OSBold";
-          ctx.textAlign = "center";
-          let oneTitle = getValue(bTwo.id, bTwo.value);
-          ctx.fillText(oneTitle, 127, 286);
-        }
-
-        // Bottom Badge
-        const badgeThreeImage = await Canvas.loadImage(
-          `https://cdn.apexstats.dev/Badges/${badgeImage(bThree.id, bThree.value)}`
-        );
-        ctx.drawImage(badgeThreeImage, 80, 321, 95, 95);
-
-        // Bottom Badge Value
-        if (hasValue(bThree.id, bThree.value)) {
-          const textThreeBackground = await Canvas.loadImage(
-            `https://cdn.apexstats.dev/Badges/textBackground.png`
+          // Top Badge
+          const badgeOneImage = await Canvas.loadImage(
+            `https://cdn.apexstats.dev/Badges/${badgeImage(bOne.id, bOne.value)}`
           );
-          ctx.drawImage(textThreeBackground, 80, 321, 95, 95);
+          ctx.drawImage(badgeOneImage, 80, 86, 95, 95);
 
+          // Top Badge Value
+          if (hasValue(bOne.id, bOne.value)) {
+            const textOneBackground = await Canvas.loadImage(
+              `https://cdn.apexstats.dev/Badges/textBackground.png`
+            );
+            ctx.drawImage(textOneBackground, 80, 86, 95, 95);
+
+            ctx.fillStyle = "#FFFFFFF";
+            ctx.font = "13px OSBold";
+            ctx.textAlign = "center";
+            let oneTitle = getValue(bOne.id, bOne.value);
+            ctx.fillText(oneTitle, 127, 170);
+          }
+
+          // Middle Badge
+          const badgeTwoImage = await Canvas.loadImage(
+            `https://cdn.apexstats.dev/Badges/${badgeImage(bTwo.id, bTwo.value)}`
+          );
+          ctx.drawImage(badgeTwoImage, 80, 201, 95, 95);
+
+          // Middle Badge Value
+          if (hasValue(bTwo.id, bTwo.value)) {
+            const textTwoBackground = await Canvas.loadImage(
+              `https://cdn.apexstats.dev/Badges/textBackground.png`
+            );
+            ctx.drawImage(textTwoBackground, 80, 201, 95, 95);
+
+            ctx.fillStyle = "#FFFFFFF";
+            ctx.font = "13px OSBold";
+            ctx.textAlign = "center";
+            let oneTitle = getValue(bTwo.id, bTwo.value);
+            ctx.fillText(oneTitle, 127, 286);
+          }
+
+          // Bottom Badge
+          const badgeThreeImage = await Canvas.loadImage(
+            `https://cdn.apexstats.dev/Badges/${badgeImage(bThree.id, bThree.value)}`
+          );
+          ctx.drawImage(badgeThreeImage, 80, 321, 95, 95);
+
+          // Bottom Badge Value
+          if (hasValue(bThree.id, bThree.value)) {
+            const textThreeBackground = await Canvas.loadImage(
+              `https://cdn.apexstats.dev/Badges/textBackground.png`
+            );
+            ctx.drawImage(textThreeBackground, 80, 321, 95, 95);
+
+            ctx.fillStyle = "#FFFFFFF";
+            ctx.font = "13px OSBold";
+            ctx.textAlign = "center";
+            let oneTitle = getValue(bThree.id, bThree.value);
+            ctx.fillText(oneTitle, 127, 406);
+          }
+
+          // Top Tracker
+          // Image
+          const oneImage = await Canvas.loadImage(
+            `https://cdn.apexstats.dev/CanvasTesting/trackerImages/${findLegendByID(
+              legend
+            )}/${trackerImage(one.id, findLegendByID(legend))}.png`
+          );
+          ctx.drawImage(oneImage, 14, 432, 237, 79);
+
+          // Title
           ctx.fillStyle = "#FFFFFFF";
-          ctx.font = "13px OSBold";
-          ctx.textAlign = "center";
-          let oneTitle = getValue(bThree.id, bThree.value);
-          ctx.fillText(oneTitle, 127, 406);
-        }
+          ctx.font = "15px OSBold";
+          ctx.textAlign = "left";
+          let oneTitle = truncate(trackerTitle(one.id, findLegendByID(legend)).toUpperCase(), 21);
+          ctx.fillText(oneTitle, 32, 458);
 
-        // Top Tracker
-        // Image
-        const oneImage = await Canvas.loadImage(
-          `https://cdn.apexstats.dev/CanvasTesting/trackerImages/${findLegendByID(
-            legend
-          )}/${trackerImage(one.id, findLegendByID(legend))}.png`
-        );
-        ctx.drawImage(oneImage, 14, 432, 237, 79);
+          // Value
+          ctx.fillStyle = "#FFFFFF";
+          ctx.font = "30px Cousine";
+          ctx.textAlign = "left";
+          let oneValue = trackerValue(one.id, one.value);
+          ctx.fillText(oneValue, 32, 495);
 
-        // Title
-        ctx.fillStyle = "#FFFFFFF";
-        ctx.font = "15px OSBold";
-        ctx.textAlign = "left";
-        let oneTitle = truncate(trackerTitle(one.id, findLegendByID(legend)).toUpperCase(), 21);
-        ctx.fillText(oneTitle, 32, 458);
+          // Middle Tracker
+          // Image
+          const twoImage = await Canvas.loadImage(
+            `https://cdn.apexstats.dev/CanvasTesting/trackerImages/${findLegendByID(
+              legend
+            )}/${trackerImage(two.id, findLegendByID(legend))}.png`
+          );
+          ctx.drawImage(twoImage, 14, 514, 237, 79);
 
-        // Value
-        ctx.fillStyle = "#FFFFFF";
-        ctx.font = "30px Cousine";
-        ctx.textAlign = "left";
-        let oneValue = trackerValue(one.id, one.value);
-        ctx.fillText(oneValue, 32, 495);
+          // Title
+          ctx.fillStyle = "#FFFFFFF";
+          ctx.font = "15px OSBold";
+          ctx.textAlign = "left";
+          let twoTitle = truncate(trackerTitle(two.id, findLegendByID(legend)).toUpperCase(), 21);
+          ctx.fillText(twoTitle, 32, 540);
 
-        // Middle Tracker
-        // Image
-        const twoImage = await Canvas.loadImage(
-          `https://cdn.apexstats.dev/CanvasTesting/trackerImages/${findLegendByID(
-            legend
-          )}/${trackerImage(two.id, findLegendByID(legend))}.png`
-        );
-        ctx.drawImage(twoImage, 14, 514, 237, 79);
+          // Value
+          ctx.fillStyle = "#FFFFFF";
+          ctx.font = "30px Cousine";
+          ctx.textAlign = "left";
+          let twoValue = trackerValue(two.id, two.value);
+          ctx.fillText(twoValue, 32, 577);
 
-        // Title
-        ctx.fillStyle = "#FFFFFFF";
-        ctx.font = "15px OSBold";
-        ctx.textAlign = "left";
-        let twoTitle = truncate(trackerTitle(two.id, findLegendByID(legend)).toUpperCase(), 21);
-        ctx.fillText(twoTitle, 32, 540);
+          // Bottom Tracker
+          // Image
+          const threeImage = await Canvas.loadImage(
+            `https://cdn.apexstats.dev/CanvasTesting/trackerImages/${findLegendByID(
+              legend
+            )}/${trackerImage(three.id, findLegendByID(legend))}.png`
+          );
+          ctx.drawImage(threeImage, 14, 596, 237, 79);
 
-        // Value
-        ctx.fillStyle = "#FFFFFF";
-        ctx.font = "30px Cousine";
-        ctx.textAlign = "left";
-        let twoValue = trackerValue(two.id, two.value);
-        ctx.fillText(twoValue, 32, 577);
+          // Title
+          ctx.fillStyle = "#FFFFFFF";
+          ctx.font = "15px OSBold";
+          ctx.textAlign = "left";
+          let threeTitle = truncate(
+            trackerTitle(three.id, findLegendByID(legend)).toUpperCase(),
+            21
+          );
+          ctx.fillText(threeTitle, 32, 622);
 
-        // Bottom Tracker
-        // Image
-        const threeImage = await Canvas.loadImage(
-          `https://cdn.apexstats.dev/CanvasTesting/trackerImages/${findLegendByID(
-            legend
-          )}/${trackerImage(three.id, findLegendByID(legend))}.png`
-        );
-        ctx.drawImage(threeImage, 14, 596, 237, 79);
+          // Value
+          ctx.fillStyle = "#FFFFFF";
+          ctx.font = "30px Cousine";
+          ctx.textAlign = "left";
+          let threeValue = trackerValue(three.id, three.value);
+          ctx.fillText(threeValue, 32, 659);
 
-        // Title
-        ctx.fillStyle = "#FFFFFFF";
-        ctx.font = "15px OSBold";
-        ctx.textAlign = "left";
-        let threeTitle = truncate(trackerTitle(three.id, findLegendByID(legend)).toUpperCase(), 21);
-        ctx.fillText(threeTitle, 32, 622);
+          // Set canvas image as attachment and send
+          const attachment = new MessageAttachment(canvas.toBuffer(), "stats.png");
 
-        // Value
-        ctx.fillStyle = "#FFFFFF";
-        ctx.font = "30px Cousine";
-        ctx.textAlign = "left";
-        let threeValue = trackerValue(three.id, three.value);
-        ctx.fillText(threeValue, 32, 659);
+          msg.delete();
+          msg.say(attachment);
+        })
+        .catch((error) => {
+          console.log("-- ERROR OUTPUT --");
+          // console.log(error);
 
-        // Set canvas image as attachment and send
-        const attachment = new MessageAttachment(canvas.toBuffer(), "stats.png");
+          if (
+            error.response.data == null ||
+            error.response.data == undefined ||
+            error.response.data == "undefined"
+          ) {
+            console.log("-- ERROR WAS NOT DEFINED --");
+            return msg.say("There was an error that was not caught. Please try again.");
+          }
 
-        msg.delete();
-        msg.say(attachment);
-      });
+          var error = error.response.data;
+
+          console.log(chalk`{red Error: ${error.error}}`);
+
+          function checkErrorType(code) {
+            if (code == 1)
+              return "**Error**\nThere was no platform and/or username specific. This shouldn't happen, so contact SDCore#1234 if you see this.";
+
+            if (code == 2)
+              return "**Error**\nThere was not a valid platform provided. Please use PC/X1/PS4.";
+
+            if (code == 3)
+              return "**Error**\nThere was an error connecting to an external API. Please try again or contact SDCore#1234 if the problem persists.";
+
+            if (code == 4)
+              return `**Error**\nUsername '${username}' on ${platform} not found. Either it is incorrect, or it doesn't exist. Try using the username of your Origin account.`;
+
+            if (code == 5)
+              return `**Error**\nUsername '${username}' on ${platform} was found, but that account hasn't played Apex. Try a different username.`;
+
+            return "**Error**\nGeneric, unhandled error. Contact SDCore#1234 if you see this.";
+          }
+
+          msg.delete();
+          msg.say(checkErrorType(error.errorCode));
+        });
     });
   }
 };
