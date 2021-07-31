24ddec8eaf67711d92bdd8c1cb643a3074868bc1
Connor <halil_ismail@yahoo.com>
2021-07-31 11:53:49 +0200

    Button(-pagination) framework

--- ApexStats.js
+++ ApexStats.js
@@ -1,6 +1,7 @@
 const Commando = require("discord.js-light-commando");
 const sqlite = require("sqlite");
 const sqlite3 = require("sqlite3");
+const buttons = require("discord-buttons");
 
 const client = new Commando.Client({
   owner: "360564818123554836",
@@ -30,19 +31,26 @@ client.registry
     ["misc", "Uncategorized"],
   ])
   .registerDefaultGroups()
-  .registerDefaultCommands({help: false, unknownCommand: false})
+  .registerDefaultCommands({ help: false, unknownCommand: false })
   .registerCommandsIn(path.join(__dirname, "commands"));
 
 client
   .setProvider(
     sqlite
-      .open({filename: "database.db", driver: sqlite3.Database})
+      .open({ filename: "database.db", driver: sqlite3.Database })
       .then((db) => new Commando.SQLiteProvider(db))
   )
   .catch(console.error);
 
 require("./events.js")(client);
 require("./rotations.js")(client);
+
+// For the Button-Framework to work
+buttons(client);
+
+// Button Pagination Framework
+const button = require('./framework/pagination')
+button(client)
 
 const config = require("./config.json");
 
