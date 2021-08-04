8222fe0405458df95da089b7acdb9b4e7fe3d755
SDCore <sdcored@gmail.com>
2021-08-04 18:42:25 -0500

    Updates

--- events/error.js
+++
@@ -1,7 +0,0 @@
-const client = require('../Apex.js');
-const chalk = require('chalk');
-const { DateTime } = require('luxon');
-
-client.once('error', error => {
-	console.log(chalk`{red [${DateTime.local().toFormat('hh:mm:ss')}] ERROR: ${error}}`);
-});
