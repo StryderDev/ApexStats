43397eb6373dd5859d8dca63092d880b13141ce0
SDCore <sdcored@gmail.com>
2021-10-18 23:17:27 -0500

    Some Error Handling

--- functions/botStatusUpdate.js
+++ functions/botStatusUpdate.js
@@ -27,58 +27,61 @@ async function updateBotStatus() {
 	let days = Math.floor(process.uptime() / 86400);
 	let hours = Math.floor(process.uptime() / 3600) % 24;
 	let minutes = Math.floor(process.uptime() / 60) % 60;
-	let seconds = Math.floor(process.uptime()) % 60;
 
 	var memoryUsage = process.memoryUsage().heapUsed / (1024 * 1024);
 
-	const apiCount = await axios.get('https://api.apexstats.dev/playerCount');
-
-	const map = new MessageEmbed()
-		.setTitle(`Apex Legends Stats Bot \`V${version}\``)
-		.setDescription(
-			`<:Uptime:896958688781283329> Uptime: ${isPlural(days, 'day')}, ${isPlural(hours, 'hour')}, ${isPlural(
-				minutes,
-				'minute',
-			)}`,
-		)
-		.addField(
-			'Bot Info',
-			`<:GuildIcon:896947200008019978> Server Count: ${servers.toLocaleString()}\n<:ShardCount:896952210171261010> Shard Count: ${
-				client.config.discord.shards
-			}`,
-			true,
-		)
-		.addField(
-			'Process Info',
-			`<:CPU:896972486766366741> CPU Usage: ${cpu
-				.usage()
-				.percent.toFixed(2)}%\n<:RAM:896972691737837598> RAM Usage: ${memoryUsage.toFixed(2)} MB`,
-			true,
-		)
-		.addField(
-			'API Info',
-			`<:UserCount:896957419840753684> Players Tracked: ${apiCount.data.count.toLocaleString()}`,
-			true,
-		)
-		.setTimestamp();
-
-	const guild = client.guilds.cache.get(botStatus.guild);
-	if (!guild) return;
-
-	const channel = guild.channels.cache.find(c => c.id === botStatus.channel && c.type === 'GUILD_TEXT');
-	if (!channel) return;
-
 	try {
-		const message = channel.messages.fetch(botStatus.message);
-		if (!message) return; // console.log('Unable to find message.');
+		const apiCount = await axios.get('https://api.apexstats.dev/playerCount');
 
-		channel.messages.fetch(botStatus.message).then(msg => {
-			msg.edit({ embeds: [map] });
-		});
+		const map = new MessageEmbed()
+			.setTitle(`Apex Legends Stats Bot \`V${version}\``)
+			.setDescription(
+				`<:Uptime:896958688781283329> Uptime: ${isPlural(days, 'day')}, ${isPlural(hours, 'hour')}, ${isPlural(
+					minutes,
+					'minute',
+				)}`,
+			)
+			.addField(
+				'Bot Info',
+				`<:GuildIcon:896947200008019978> Server Count: ${servers.toLocaleString()}\n<:ShardCount:896952210171261010> Shard Count: ${
+					client.config.discord.shards
+				}`,
+				true,
+			)
+			.addField(
+				'Process Info',
+				`<:CPU:896972486766366741> CPU Usage: ${cpu
+					.usage()
+					.percent.toFixed(2)}%\n<:RAM:896972691737837598> RAM Usage: ${memoryUsage.toFixed(2)} MB`,
+				true,
+			)
+			.addField(
+				'API Info',
+				`<:UserCount:896957419840753684> Players Tracked: ${apiCount.data.count.toLocaleString()}`,
+				true,
+			)
+			.setTimestamp();
 
-		console.log(chalk`{blue.bold [${DateTime.local().toFormat('hh:mm:ss')}] Updated Bot Status Embed}`);
+		const guild = client.guilds.cache.get(botStatus.guild);
+		if (!guild) return;
+
+		const channel = guild.channels.cache.find(c => c.id === botStatus.channel && c.type === 'GUILD_TEXT');
+		if (!channel) return;
+
+		try {
+			const message = channel.messages.fetch(botStatus.message);
+			if (!message) return; // console.log('Unable to find message.');
+
+			channel.messages.fetch(botStatus.message).then(msg => {
+				msg.edit({ embeds: [map] });
+			});
+
+			console.log(chalk`{blue.bold [${DateTime.local().toFormat('hh:mm:ss')}] Updated Bot Status Embed}`);
+		} catch (err) {
+			console.error(`Other Error: ${err}`);
+		}
 	} catch (err) {
-		console.error(`Other Error: ${err}`);
+		console.log(`Axios Error: ${err}`);
 	}
 }
 
