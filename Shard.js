b9d6d08fb02e6e73e0a601946d2e7e519fd65704
SDCore <sdcored@gmail.com>
2021-11-17 04:18:15 -0600

    TopGG

--- Shard.js
+++ Shard.js
@@ -2,6 +2,7 @@ const { ShardingManager } = require('discord.js');
 const config = require('./config.json');
 const chalk = require('chalk');
 const { DateTime } = require('luxon');
+const { AutoPoster } = require('topgg-autoposter');
 
 const timeLogs = DateTime.local().toFormat('hh:mm:ss');
 
@@ -9,6 +10,14 @@ const manager = new ShardingManager('./Apex.js', {
 	totalShards: config.discord.shards,
 	token: config.discord.token,
 });
+
+if (config.debug == false) {
+	const poster = AutoPoster(config.topgg, manager);
+
+	poster.on('posted', stats => {
+		console.log(`Posted stats to Top.gg | ${stats.serverCount} servers`);
+	});
+}
 
 manager.on('shardCreate', shard => console.log(chalk`{blue.bold [${timeLogs}] Shard Launched. ID: ${shard.id}}`));
 
