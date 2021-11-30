5e83fd48dd3b21ccbb307e6a74b934578b3e04d6
SDCore <sdcored@gmail.com>
2021-11-30 04:36:57 -0600

    Edit Ping Command

--- slash/ping.js
+++ slash/ping.js
@@ -3,12 +3,12 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
 	data: new SlashCommandBuilder().setName('ping').setDescription('Show current websocket and message latency.'),
 	async execute(client, interaction) {
-		await interaction.reply({ content: 'Detecting API and Message Latency...' }).then(i =>
-			interaction.editReply({
-				content: `**API Latency:** \`${Math.round(client.ws.ping)}ms\`\n**Message Latency:** \`${
-					Date.now() - interaction.createdTimestamp
-				}ms\``,
-			}),
-		);
+		await interaction.reply({ content: 'Detecting API and Message Latency...' });
+
+		await interaction.editReply({
+			content: `**API Latency:** \`${Math.round(client.ws.ping)}ms\`\n**Message Latency:** \`${
+				Date.now() - interaction.createdTimestamp
+			}ms\``,
+		});
 	},
 };
