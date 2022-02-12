af80dbd46291338b841065525240fd6a382e767e
SDCored <sdcored@gmail.com>
2022-02-12 04:23:44 -0600

    Who Command

--- commands/who.js
+++ commands/who.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed } = require('discord.js');
+const wait = require('util').promisify(setTimeout);
 
 module.exports = {
 	data: new SlashCommandBuilder().setName('who').setDescription('Pick a random legend to play.'),
@@ -8,6 +9,36 @@ module.exports = {
 			`<a:ApexBot_Loading:940037271980220416> Choosing a random legend...`,
 		);
 
+		const legends = [
+			'Bloodhound',
+			'Gibraltar',
+			'Lifeline',
+			'Pathfinder',
+			'Wraith',
+			'Bangalore',
+			'Caustic',
+			'Mirage',
+			'Octane',
+			'Wattson',
+			'Crypto',
+			'Revenant',
+			'Loba',
+			'Rampart',
+			'Horizon',
+			'Fuse',
+			'Valkyrie',
+			'Seer',
+			'Ash',
+			'Mad Maggie',
+		];
+
+		const legend = Math.floor(Math.random() * legends.length);
+		const legendEmbed = new MessageEmbed()
+			.setDescription(`Play **${legends[legend]}** this round!`)
+			.setImage(`https://cdn.apexstats.dev/Bot/Legends/Banners/${legends[legend]}.png`);
+
 		await interaction.reply({ embeds: [loadingEmbed] });
+		await wait(1000);
+		await interaction.editReply({ embeds: [legendEmbed] });
 	},
 };
