1c0ef48e74706dbd41e91cc54df500a291027f30
SDCored <sdcored@gmail.com>
2022-03-19 03:05:01 -0500

    Legend Types in /who Command

--- commands/who.js
+++ commands/who.js
@@ -3,8 +3,23 @@ const { MessageEmbed } = require('discord.js');
 const wait = require('util').promisify(setTimeout);
 
 module.exports = {
-	data: new SlashCommandBuilder().setName('who').setDescription('Pick a random legend to play.'),
+	data: new SlashCommandBuilder()
+		.setName('who')
+		.setDescription('Pick a random legend to play.')
+		.addStringOption(option =>
+			option
+				.setName('type')
+				.setDescription('Category of legends to choose from.')
+				.setRequired(false)
+				.addChoice('Offensive', 'Offensive')
+				.addChoice('Defensive', 'Defensive')
+				.addChoice('Support', 'Support')
+				.addChoice('Recon', 'Recon'),
+		),
 	async execute(interaction) {
+		// Options
+		const type = interaction.options.getString('type');
+
 		const loadingEmbed = new MessageEmbed().setDescription(
 			`<a:ApexBot_Loading:940037271980220416> Choosing a random legend...`,
 		);
@@ -32,13 +47,51 @@ module.exports = {
 			'Mad Maggie',
 		];
 
-		const legend = Math.floor(Math.random() * legends.length);
-		const legendEmbed = new MessageEmbed()
-			.setDescription(`Play **${legends[legend]}** this round!`)
-			.setImage(`https://cdn.apexstats.dev/Bot/Legends/Banners/${encodeURIComponent(legends[legend])}.png`);
+		const offensive = [
+			'Wraith',
+			'Bangalore',
+			'Mirage',
+			'Octane',
+			'Revenant',
+			'Horizon',
+			'Fuse',
+			'Ash',
+			'Mad Maggie',
+		];
 
-		await interaction.editReply({ embeds: [loadingEmbed] });
-		await wait(1000);
-		await interaction.editReply({ embeds: [legendEmbed] });
+		const defensive = ['Gibraltar', 'Caustic', 'Wattson', 'Rampart'];
+
+		const support = ['Lifeline', 'Loba'];
+
+		const recon = ['Bloodhound', 'Pathfinder', 'Crypto', 'Valkyrie', 'Seer'];
+
+		if (!type) {
+			const legend = Math.floor(Math.random() * legends.length);
+			const legendEmbed = new MessageEmbed()
+				.setDescription(`Play **${legends[legend]}** this round!`)
+				.setImage(`https://cdn.apexstats.dev/Bot/Legends/Banners/${encodeURIComponent(legends[legend])}.png`);
+
+			await interaction.editReply({ embeds: [loadingEmbed] });
+			await wait(1000);
+			await interaction.editReply({ embeds: [legendEmbed] });
+		} else {
+			function legendType(type) {
+				if (type == 'Offensive') return offensive;
+				if (type == 'Defensive') return defensive;
+				if (type == 'Support') return support;
+				if (type == 'Recon') return recon;
+			}
+
+			const legend = Math.floor(Math.random() * legendType(type).length);
+			const legendEmbed = new MessageEmbed()
+				.setDescription(`Play **${legendType(type)[legend]}** this round!`)
+				.setImage(
+					`https://cdn.apexstats.dev/Bot/Legends/Banners/${encodeURIComponent(legendType(type)[legend])}.png`,
+				);
+
+			await interaction.editReply({ embeds: [loadingEmbed] });
+			await wait(1000);
+			await interaction.editReply({ embeds: [legendEmbed] });
+		}
 	},
 };
