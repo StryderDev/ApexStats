6ac085f1bfcfb47b07285c1259bcf110a3270dab
SDCored <sdcored@gmail.com>
2022-03-20 05:49:48 -0500

    Add TopGG Autoposter

--- Shard.js
+++ Shard.js
@@ -2,6 +2,7 @@ const { ShardingManager } = require('discord.js');
 const { discord } = require('./config.json');
 
 const chalk = require('chalk');
+const { AutoPoster } = require('topgg-autoposter');
 
 const manager = new ShardingManager('./Apex.js', { token: discord.token });
 
@@ -9,5 +10,13 @@ manager.on('shardCreate', shard => {
 	console.log(chalk`{yellow.bold [> Launching Shard...]}`);
 	console.log(chalk`{green.bold [> Shard #${shard.id + 1} Launched]}`);
 });
+
+if (discord.debug == false) {
+	const poster = AutoPoster('topggtoken', sharder);
+
+	poster.on('posted', stats => {
+		console.log(`Posted stats to Top.gg | ${stats.serverCount} servers`);
+	});
+}
 
 manager.spawn();
