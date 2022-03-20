3123495882e07122d0ab66450b729af243a2e91a
SDCored <sdcored@gmail.com>
2022-03-20 01:41:13 -0500

    Updated Info Command
    - Added shard info/uptime
    - Reorganized fields, added Ko-Fi link
    - Added how to add bot to server in the footer

--- commands/info.js
+++ commands/info.js
@@ -7,16 +7,20 @@ const { version } = require('../package.json');
 module.exports = {
 	data: new SlashCommandBuilder().setName('info').setDescription('Shows info about the bot.'),
 	async execute(interaction) {
+		const uptime = process.uptime();
+		const seconds = Math.floor(uptime % 60);
+		const minutes = Math.floor(uptime / 60);
+		const hours = Math.floor(uptime / 3600);
+		const days = Math.floor(uptime / 86400);
+
 		const info = new MessageEmbed()
 			.setTitle('Apex Legends Stats Bot')
-			.setDescription('Legend stats, map info, and random legend picker.')
-			.addField('Version', `v${version} "${release.name}"`, true)
-			.addField('Shard', `ID: ${interaction.client.shard.ids[0] + 1}`, true)
-			.addField(
-				'Links',
-				'[GitHub](https://github.com/sdcore/apex-stats-bot)\n[Support Server](https://discord.gg/eH8VxssFW6)',
-				true,
-			);
+			.setDescription('Get user stats, Battle Royale and Arenas map rotations, news, and more. Start by typing `/` for a list of commands.')
+			.setThumbnail('https://cdn.apexstats.dev/Bot/Avatar/2022/Season%2012.png')
+			.addField('Links', '[Ko-Fi](https://ko-fi.com/sdcore)\n[GitHub](https://github.com/sdcore/apex-stats-bot)\n[Support Server](https://discord.gg/eH8VxssFW6)', true)
+			.addField('Bot Info', `Shard ${interaction.client.shard.ids[0] + 1}/${interaction.client.shard.count}\nVersion ${version}\nCodename "${release.name}"`, true)
+			.addField('Uptime', `${days}d, ${hours}h, ${minutes}m, ${seconds}s`, true)
+			.setFooter({ text: 'Wanna add the bot to your server? Click the "Apex Stats" username and press "Add to Server"' });
 
 		await interaction.editReply({ embeds: [info] });
 	},
