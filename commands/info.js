2e92f87d22c4406e9400d143defb28c82ebcf5c0
SDCored <sdcored@gmail.com>
2022-03-20 05:41:45 -0500

    Fix time

--- commands/info.js
+++ commands/info.js
@@ -9,8 +9,8 @@ module.exports = {
 	async execute(interaction) {
 		const uptime = process.uptime();
 		const seconds = Math.floor(uptime % 60);
-		const minutes = Math.floor(uptime / 60);
-		const hours = Math.floor(uptime / 3600);
+		const minutes = Math.floor((uptime % (60 * 60)) / 60);
+		const hours = Math.floor(uptime / (60 * 60)) % 24;
 		const days = Math.floor(uptime / 86400);
 
 		const info = new MessageEmbed()
