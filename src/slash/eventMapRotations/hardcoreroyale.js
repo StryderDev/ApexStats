185bfe38072369d41616d477c1150a59f4c4079a
SDCore <sdcored@gmail.com>
2023-01-25 03:54:01 -0600

    Hardcore Royale Map Command

--- src/slash/eventMapRotations/hardcoreroyale.js
+++ src/slash/eventMapRotations/hardcoreroyale.js
@@ -5,15 +5,11 @@ const { api } = require('../../config.json');
 const { Misc } = require('../../data/emotes.json');
 
 module.exports = {
-	data: new SlashCommandBuilder().setName('control').setDescription('Shows the current in-game control map.'),
+	data: new SlashCommandBuilder().setName('hardcoreroyale').setDescription('Shows the current in-game Hardcore Royale map.'),
 	async execute(interaction) {
-		const loadingEmbed = new EmbedBuilder().setDescription(`${Misc.Loading} Loading current in-game Control map...`).setColor('2F3136');
+		const loadingEmbed = new EmbedBuilder().setDescription(`${Misc.Loading} Loading current in-game Hardcore Royale map...`).setColor('2F3136');
 
 		await interaction.editReply({ embeds: [loadingEmbed] });
-
-		function mapImage(map) {
-			if (map == 'Hammond Labs After Dark') return 'Hammond Labs';
-		}
 
 		function isPlural(number, word) {
 			if (number != 1) return `${word}s`;
@@ -35,17 +31,17 @@ module.exports = {
 		await axios
 			.get(`https://api.mozambiquehe.re/maprotation?version=5&auth=${api.apex}`)
 			.then(response => {
-				const control = response.data.ltm;
+				const hardcore = response.data.ltm;
 
 				const mapEmbed = new EmbedBuilder()
-					.setTitle(`Legends are currently Taking Control in **${control.current.map}**.`)
+					.setTitle(`Legends are currently dropping into **${hardcore.current.map}**.`)
 					.setDescription(
-						`${control.current.map} ends <t:${control.current.end}:R>, or at <t:${control.current.end}:t>.\n**Next up:** ${control.next.map} for ${mapLength(
-							control.next.DurationInMinutes,
+						`${hardcore.current.map} ends <t:${hardcore.current.end}:R>, or at <t:${hardcore.current.end}:t>.\n**Next up:** ${hardcore.next.map} for ${mapLength(
+							hardcore.next.DurationInMinutes,
 						)}.`,
 					)
 					.setColor('2F3136')
-					.setImage(`https://cdn.jumpmaster.xyz/Bot/Maps/Season%2015/Control/${encodeURIComponent(control.current.map)}.png`);
+					.setImage(`https://cdn.jumpmaster.xyz/Bot/Maps/Season%2015/Battle%20Royale/${encodeURIComponent(hardcore.current.map)}.png`);
 
 				interaction.editReply({ embeds: [mapEmbed] });
 			})
