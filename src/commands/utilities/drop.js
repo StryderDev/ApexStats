7f4ff2a25031ce9b2f02c27bc2f7c7ebaf23b7a6
SDCore <sdcored@gmail.com>
2023-02-14 00:39:06 -0600

    Finish Drop Command

--- src/commands/utilities/drop.js
+++ src/commands/utilities/drop.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 const wait = require('util').promisify(setTimeout);
 const { EmbedBuilder, SlashCommandBuilder } = require('discord.js');
 
@@ -32,7 +33,51 @@ module.exports = {
 		await wait(1000);
 
 		if (mapOption != null) {
-			const map = `../../data/drops/Season 16/${mapOption}.json`;
+			const mapFile = require(`../../data/drops/Season 16/${mapOption}.json`);
+			const map = Math.floor(Math.random() * mapFile.length);
+
+			interaction.editReply({ content: `Drop into ${mapFile[map]} on ${mapOption}!`, embeds: [] });
+		} else {
+			await axios
+				.get('https://api.jumpmaster.xyz/map/')
+				.then(response => {
+					const br = response.data.br.map;
+
+					const mapFile = require(`../../data/drops/Season 16/${br.name}.json`);
+					const map = Math.floor(Math.random() * mapFile.length);
+
+					interaction.editReply({ content: `Drop into ${mapFile[map]} on ${br.name}`, embeds: [] });
+				})
+				.catch(error => {
+					if (error.response) {
+						console.log(error.response.data);
+
+						const errorEmbed = new EmbedBuilder().setTitle('Map Lookup Error').setDescription(error.response.data.error).setColor('D0342C').setTimestamp();
+
+						axios.get(`https://api.jumpmaster.xyz/logs/Stats?type=error&dev=${debug}`);
+
+						interaction.editReply({ embeds: [errorEmbed] });
+					} else if (error.request) {
+						console.log(error.request);
+
+						const errorEmbed = new EmbedBuilder()
+							.setTitle('Site Lookup Error')
+							.setDescription(`The request was not returned successfully.\nThis is potentially an error with the API.\nPlease try again shortly.`)
+							.setColor('D0342C')
+							.setTimestamp();
+
+						interaction.editReply({ embeds: [errorEmbed] });
+					} else {
+						console.log(error.message);
+
+						const errorEmbed = new EmbedBuilder()
+							.setTitle('Unknown Error')
+							.setDescription(`This should never happen.\nIf you see this error, please contact <@360564818123554836> ASAP.`)
+							.setColor('D0342C');
+
+						interaction.editReply({ embeds: [errorEmbed] });
+					}
+				});
 		}
 	},
 };
