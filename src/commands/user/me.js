a6366abcc91ef750f85ddfa69a2e67f50f0971a4
SDCore <sdcored@gmail.com>
2023-07-26 03:36:27 -0500

    Update /me Stats

--- src/commands/user/me.js
+++ src/commands/user/me.js
@@ -4,7 +4,7 @@ const db = require('../../utilities/database.js');
 const { EmbedBuilder, SlashCommandBuilder } = require('discord.js');
 
 const { embedColor, Account, Misc } = require('../../data/utilities.json');
-const { getStatus, rankLayout, battlepass, platformName, platformEmote } = require('../../utilities/stats.js');
+const { getStatus, battlepass, platformName, platformEmote, checkUserBan, calcTillMaster, calcTillPred, getRankName, getDivisionCount } = require('../../utilities/stats.js');
 
 module.exports = {
 	data: new SlashCommandBuilder().setName('me').setDescription('Shows the stats of your linked Apex account.'),
@@ -18,7 +18,7 @@ module.exports = {
 
 		const discordID = interaction.user.id;
 
-		db.query(linkQuery, [discordID], (err, row) => {
+		db.query(linkQuery, [discordID], async (err, row) => {
 			if (err) {
 				console.log(err);
 				return interaction.editReply({ content: 'There was a database error.', embeds: [] });
@@ -28,74 +28,105 @@ module.exports = {
 				return interaction.editReply({ content: `You do not have a linked account. Use \`/link\` to link an Apex account to your Discord account.`, embeds: [] });
 			} else {
 				// User already has an account linked
-				axios
-					.get(`https://api.jumpmaster.xyz/user/Player?platform=${row[0].platform}&ID=${encodeURIComponent(row[0].playerID)}&key=${process.env.SPYGLASS}`)
-					.then(response => {
-						const data = response.data;
+				const playerAPI = axios.get(
+					`https://api.jumpmaster.xyz/user/Player?platform=${row[0].platform}&ID=${encodeURIComponent(row[0].playerID)}&key=${process.env.SPYGLASS}`,
+				);
+				const predAPI = axios.get(`https://api.jumpmaster.xyz/misc/predThreshold`);
 
-						// User Data
-						const user = data.user;
-						const legend = data.active.legend;
-						const status = user.status;
-						const ranked = data.ranked.BR;
-						const account = data.account;
+				await axios
+					.all([playerAPI, predAPI])
+					.then(
+						axios.spread((...res) => {
+							// Grabbing the data from the axios requests
+							const playerData = res[0].data;
+							const predData = res[1].data;
 
-						// Trackers
-						const trackers = data.active.trackers;
+							// Main user data
+							const user = playerData.user;
+							const legend = playerData.active.legend;
+							const status = user.status;
+							const account = playerData.account;
+							const ranked = playerData.ranked.BR;
+							const trackers = playerData.active.trackers;
 
-						// Calculate account, prestige, and battle pass level completion
-						const accountCompletion = Math.floor((account.level.current / 500) * 100);
-						const prestigeCompletion = Math.floor((account.level.total / 2000) * 100);
-						const battlepassCompletion = Math.floor((battlepass(account.battlepass) / 110) * 100);
+							// Calculate account, prestige, and battle pass level completion
+							const accountCompletion = Math.floor((account.level.current / 500) * 100);
 
-						// Stats Embed
-						const stats = new EmbedBuilder()
-							.setTitle(`${platformEmote(user.platform)} ${user.username} playing ${legend}`)
-							.setDescription(`[**Status:** ${getStatus(status)}]`)
-							.addFields([
-								{
-									name: `${Account.Level} Account`,
-									value: `${Misc.GrayBlank} Level ${account.level.current.toLocaleString()} (${accountCompletion}%)\n${Misc.GrayBlank} Prestige ${
-										account.level.prestige
-									} (${prestigeCompletion}%)`,
-									inline: true,
-								},
-								{
-									name: `${Account.BattlePass} Revelry Battle Pass`,
-									value: `${Misc.GrayBlank} Level ${battlepass(account.battlepass)} (${battlepassCompletion}%)`,
-									inline: true,
-								},
-								{
-									name: `Battle Royale Ranked`,
-									value: `${rankLayout(ranked)}\n\n**Active Trackers**`,
-								},
-								{
-									name: trackers[0].id,
-									value: trackers[0].value.toLocaleString(),
-									inline: true,
-								},
-								{
-									name: trackers[1].id,
-									value: trackers[1].value.toLocaleString(),
-									inline: true,
-								},
-								{
-									name: trackers[2].id,
-									value: trackers[2].value.toLocaleString(),
-									inline: true,
-								},
-							])
-							.setImage(`https://cdn.jumpmaster.xyz/Bot/Legends/Banners/${encodeURIComponent(legend)}.png?t=${Math.floor(Math.random() * 10)}`)
-							.setColor(embedColor)
-							.setFooter({
-								text: `Player Added: ${new Date(user.userAdded * 1000).toUTCString()}\nEquip the Battle Pass badge to update it!`,
-							});
+							function checkPrestigeCompletion(amount) {
+								if (amount >= 2000) {
+									return 2000;
+								} else {
+									return amount;
+								}
+							}
 
-						// Logging
-						// axios.get(`https://api.jumpmaster.xyz/logs/Stats?type=success&dev=${debug}`);
+							const prestigeCompletion = Math.floor((checkPrestigeCompletion(account.level.total) / 2000) * 100);
 
-						interaction.editReply({ embeds: [stats] });
-					})
+							const battlepassCompletion = Math.floor((battlepass(account.battlepass) / 110) * 100);
+
+							const stats = new EmbedBuilder()
+								.setTitle(`${platformEmote(row[0].platform)} ${user.username} playing ${legend}`)
+								.setDescription(`**Status:** ${getStatus(status)}\n${checkUserBan(user.bans)}`)
+								.addFields([
+									{
+										name: `${Account.Level} Account`,
+										value: `${Misc.GrayBlank} Level ${account.level.current.toLocaleString()} (${accountCompletion}%)\n${Misc.GrayBlank} Prestige ${
+											account.level.prestige
+										} (${prestigeCompletion}%)`,
+										inline: true,
+									},
+									{
+										name: `${Account.BattlePass} Arsenal Battle Pass`,
+										value: `${Misc.GrayBlank} Level ${battlepass(account.battlepass)} (${battlepassCompletion}%)`,
+										inline: true,
+									},
+									{
+										name: `\u200b`,
+										value: `**Battle Royale Ranked**`,
+									},
+									{
+										name: getRankName(ranked),
+										value: `${Misc.GrayBlank} Division: ${getDivisionCount(ranked)}/1000 LP\n${Misc.GrayBlank} Total: ${ranked.score.toLocaleString()} LP`,
+										inline: true,
+									},
+									{
+										name: `\u200b`,
+										value: `${Misc.GrayBlank} Till Master: ${calcTillMaster(ranked)}\n${Misc.GrayBlank} Till Apex Predator: ${calcTillPred(
+											ranked,
+											predData,
+											row[0].platform,
+										)}`,
+										inline: true,
+									},
+									{
+										name: `\u200b`,
+										value: '**Active Trackers**',
+									},
+									{
+										name: trackers[0].id.toString(),
+										value: trackers[0].value.toLocaleString(),
+										inline: true,
+									},
+									{
+										name: trackers[1].id.toString(),
+										value: trackers[1].value.toLocaleString(),
+										inline: true,
+									},
+									{
+										name: trackers[2].id.toString(),
+										value: trackers[2].value.toLocaleString(),
+										inline: true,
+									},
+								])
+								.setImage(`https://cdn.jumpmaster.xyz/Bot/Legends/Banners/${encodeURIComponent(legend)}.png?t=${Math.floor(Math.random() * 10)}}`)
+								.setColor(embedColor)
+								.setFooter({
+									text: `Player Added: ${new Date(user.userAdded * 1000).toUTCString()}\nEquip the Battle Pass badge in-game to update it!`,
+								});
+
+							interaction.editReply({ embeds: [stats] });
+						}),
+					)
 					.catch(error => {
 						if (error.response) {
 							console.log(error.response.data);
