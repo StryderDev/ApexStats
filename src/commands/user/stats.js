7f09d3ce0fc81a07ef18c03c0869cfda978e8f67
SDCore <sdcored@gmail.com>
2024-12-17 16:20:42 -0600

    Season 23 Split 2 Changes

--- src/commands/user/stats.js
+++ src/commands/user/stats.js
@@ -90,7 +90,7 @@ module.exports = {
 								inline: true,
 							},
 							{
-								name: `${Account.BattlePass} From the Rift Split 1 Battle Pass`,
+								name: `${Account.BattlePass} From the Rift Split 2 Battle Pass`,
 								value: `${battlepass(account.battlepass)}`,
 								inline: true,
 							},
