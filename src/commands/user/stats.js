b2e8cfde5285f39a3b9f8e990a3a5e6dd9fff7f5
SDCore <sdcored@gmail.com>
2025-09-16 12:35:49 -0500

    Season 26 Split 2 Prep

--- src/commands/user/stats.js
+++ src/commands/user/stats.js
@@ -90,7 +90,7 @@ module.exports = {
 								inline: true,
 							},
 							{
-								name: `${Account.BattlePass} Showdown Split 1 Battle Pass`,
+								name: `${Account.BattlePass} Showdown Split 2 Battle Pass`,
 								value: `${battlepass(account.battlepass)}`,
 								inline: true,
 							},
