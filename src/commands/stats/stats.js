b0e901daaeb5619033ea74a0cf55487b18822706
SDCore <sdcored@gmail.com>
2025-10-15 19:25:20 -0500

    Work on Tracker Image Stuff

--- src/commands/stats/stats.js
+++ src/commands/stats/stats.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const { emoteFile } = require('../../utilities/misc.js');
+const { createCanvas, loadImage } = require('@napi-rs/canvas');
 const {
 	levelBadge,
 	formatScore,
@@ -13,7 +14,19 @@ const {
 	battlepassProgress,
 	calcDailyBPLevelsTillCompletion,
 } = require('../../utilities/stats.js');
-const { ButtonStyle, MessageFlags, ButtonBuilder, SectionBuilder, MediaGalleryItem, ContainerBuilder, TextDisplayBuilder, MediaGalleryBuilder, SlashCommandBuilder, SeparatorSpacingSize } = require('discord.js');
+const {
+	ButtonStyle,
+	MessageFlags,
+	ButtonBuilder,
+	SectionBuilder,
+	MediaGalleryItem,
+	ContainerBuilder,
+	TextDisplayBuilder,
+	MediaGalleryBuilder,
+	SlashCommandBuilder,
+	SeparatorSpacingSize,
+	AttachmentBuilder,
+} = require('discord.js');
 
 const emotes = require(`../../data/${emoteFile(process.env.DEBUG)}Emotes.json`);
 
@@ -47,6 +60,8 @@ module.exports = {
 		const seasonAPI = axios.get('https://api.jumpmaster.xyz/seasons/Current?version=2');
 		const rankedAPI = axios.get('https://api.jumpmaster.xyz/misc/predThreshold');
 
+		const trackerBackground = await loadImage('https://specter.apexstats.dev/ApexStats/Legends/Trackers/Background_3.png?key=LuH8KT5TxF5tPlQq9xVqkrNSxdPnwWYc');
+
 		const loadingContainer = new ContainerBuilder();
 
 		const legendBanner = new MediaGalleryBuilder().addItems([
@@ -78,6 +93,15 @@ module.exports = {
 			].join('\n'),
 		);
 
+		const trackerStatsBackground = new MediaGalleryBuilder().addItems([
+			{
+				type: MediaGalleryItem,
+				media: {
+					url: `https://specter.apexstats.dev/ApexStats/Legends/Trackers/Background_3.png?key=${process.env.SPECTER}`,
+				},
+			},
+		]);
+
 		const footerText = new TextDisplayBuilder().setContent(`-# Equip the Battle Pass badge in-game to update it!\n-# Equip trackers in-game to update stats`);
 
 		const profileButton = new ButtonBuilder().setLabel('View Stats Profile').setStyle(ButtonStyle.Link).setURL('https://apexstats.dev/').setDisabled(true);
@@ -103,6 +127,10 @@ module.exports = {
 
 		loadingContainer.addSectionComponents(battlepassSection);
 		loadingContainer.addSectionComponents(rankedSection);
+
+		loadingContainer.addSeparatorComponents(separator => separator.setSpacing(SeparatorSpacingSize.Small));
+
+		loadingContainer.addMediaGalleryComponents(trackerStatsBackground);
 
 		loadingContainer.addSeparatorComponents(separator => separator.setSpacing(SeparatorSpacingSize.Small));
 
@@ -179,6 +207,39 @@ module.exports = {
 						thumbnail.setURL(`https://specter.apexstats.dev/ApexStats/Banners/Ranked/${encodeURIComponent(rankBadgeImageName(playerData.ranked.BR))}.png?key=${process.env.SPECTER}`),
 					);
 
+				// Tracker Stats Image
+				const canvas = createCanvas(1200, 100);
+				const ctx = canvas.getContext('2d');
+
+				const sections = [{ title: 'Tracker 1', subtitle: 'Value 1' }];
+
+				const sectionWidth = 1200 / sections.length;
+				const padding = 20;
+				const baseY = 25;
+				const titleSize = 18;
+				const subtitleSize = 25;
+
+				ctx.drawImage(trackerBackground, 0, 0, 1200, 100);
+
+				sections.forEach((section, i) => {
+					const baseX = 1;
+
+					ctx.fillText(section.title, baseX + padding, baseY);
+				});
+
+				const imageBuffer = canvas.toBuffer('image/png');
+
+				const attachment = new AttachmentBuilder(imageBuffer, { name: 'trackerStats.png' });
+
+				const trackerImage = new MediaGalleryBuilder().addItems([
+					{
+						type: MediaGalleryItem,
+						media: {
+							url: `attachment://${attachment.name}`,
+						},
+					},
+				]);
+
 				const footerSection = new SectionBuilder().addTextDisplayComponents(footerText).setButtonAccessory(profileButton);
 
 				statsContainer.addMediaGalleryComponents(legendBanner);
@@ -191,9 +252,14 @@ module.exports = {
 
 				statsContainer.addSeparatorComponents(separator => separator.setSpacing(SeparatorSpacingSize.Small));
 
+				statsContainer.addMediaGalleryComponents(trackerImage);
+
+				statsContainer.addSeparatorComponents(separator => separator.setSpacing(SeparatorSpacingSize.Small));
+
 				statsContainer.addSectionComponents(footerSection);
 
 				interaction.editReply({
+					files: [attachment],
 					components: [statsContainer],
 					flags: MessageFlags.IsComponentsV2,
 				});
