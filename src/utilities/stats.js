aed011ca2173733cfaf90f951bbba8ffa4712f1a
SDCore <sdcored@gmail.com>
2026-06-23 11:02:23 -0500

    Overclocked Split 2 Prep

--- src/utilities/stats.js
+++ src/utilities/stats.js
@@ -39,20 +39,20 @@ function getStatus(status) {
 }
 
 function battlepass(data) {
-	if (data.history['Overclocked_1'] >= 60) {
+	if (data.history['Overclocked_2'] >= 60) {
 		rewardLevel = 60;
 		rewardCompletion = 100;
 	} else {
-		rewardLevel = data.history['Overclocked_1'];
-		rewardCompletion = Math.floor((data.history['Overclocked_1'] / 60) * 100);
+		rewardLevel = data.history['Overclocked_2'];
+		rewardCompletion = Math.floor((data.history['Overclocked_2'] / 60) * 100);
 	}
 
-	if (data.history['Overclocked_1'] >= 100) {
+	if (data.history['Overclocked_2'] >= 100) {
 		badgeLevel = 100;
 		badgeCompletion = 100;
 	} else {
-		badgeLevel = data.history['Overclocked_1'];
-		badgeCompletion = Math.floor((data.history['Overclocked_1'] / 100) * 100);
+		badgeLevel = data.history['Overclocked_2'];
+		badgeCompletion = Math.floor((data.history['Overclocked_2'] / 100) * 100);
 	}
 
 	return `${Misc.GrayBlank} Reward Completion: ${rewardLevel}/60 (${rewardCompletion}%)\n${Misc.GrayBlank} Badge Completion: ${badgeLevel}/100 (${badgeCompletion}%)`;
